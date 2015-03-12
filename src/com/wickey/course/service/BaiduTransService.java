49c09d3ee2b310d28414acc3b7155385e18ad9c1
394580093@qq.com <394580093@qq.com>
2015-03-12 15:09:11 +0800

    Revert "Revert "分支test""
    This reverts commit 651db25515412851f3c0af5e05475e6db2a9ddbb.

--- src/com/wickey/course/service/BaiduTransService.java
+++ src/com/wickey/course/service/BaiduTransService.java
@@ -71,5 +71,17 @@ public class BaiduTransService {
 		}
 		return result;
 	}
+	
+	public static String urlEncodeUTF82(String source){
+		String result = source;
+		try {
+			result = java.net.URLEncoder.encode(source, "utf-8"); 
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
 
 }
