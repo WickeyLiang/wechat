b9f4958f036965aa620cfbaff970d7d7c01ea157
394580093@qq.com <394580093@qq.com>
2015-03-13 11:36:45 +0800

    test1

--- src/com/wickey/course/bean/paramsAPI/ParaAPI.java
+++ src/com/wickey/course/bean/paramsAPI/ParaAPI.java
@@ -5,16 +5,16 @@ public class ParaAPI {
 	// apptoken
 	public static String token = "wickeyLiangweixin";
 	// token
-	public static String token_crop = "wickeyLiangweixin";
+	public static String token_crop = "";
 	
 	
 	// 随机AESKEY 1.订阅号 2.企业号第三方app
 	public static String encodingAESKey = "MUlQy5azwVXKjvHrjI1gGafRUxSeSvprwrfSFxNlQJu";
-	public static String encodingAESKey_crop = "py8GurbRbfUiaTRWtiABJDBnNuMKc7oE0thaVabEsm2";
+	public static String encodingAESKey_crop = "";
 	
 	
 	// 企业号ID
-	public static String corpId = "wx722acaeb3d700406";
+	public static String corpId = "";
 	// appID 1.个人订阅号 2.测试号 3.企业号第三方应用
 	public static String appId = "wx26370891564f6f7f";
 	public static String appId_test = "wx668290fdbd99f4d2";
@@ -22,7 +22,7 @@ public class ParaAPI {
 	
 	
 	// 管理组的凭证密钥，每个secret代表了对应用、通讯录、接口的不同权限；不同的管理组拥有不同的secret
-	public static String secret = "lOuDZZJnXbOO35ZyswntrawbH_oXOwT2oyrQntZ4Te4tcgGO0V_mAW5HFWuji0Tx";
+	public static String secret = "";
 	// 1.订阅号 2.测试号 3.企业号第三方应用
 	public static String appSecret = "74e670576d6abbcd5d40f45d758a4490";
 	public static String appSecret_test = "0a721e7a3751fa33525ac0c2aac855d1";
